4b4090567d53d1d4df78fa2a45fca36ffb573858
Nicolas97431 <nickogsm@gmail.com>
2024-09-20 15:31:16 +0400

    3eme commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/UA-front-V2/",
   //trois ligne pour IDE sinon cela ne fonctionne pas
   //  server: {
   //   port: 9000,
