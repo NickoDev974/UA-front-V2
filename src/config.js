710eebbcdd6d0960bd22d125f0b027273e9a1c85
Nicolas97431 <nickogsm@gmail.com>
2024-09-22 17:06:24 +0400

    connection back

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const config = {
-  api_url: "http://UA.localhost:9500", //"http://nicolasbienes_UA.ide.3wa.io:9500",
-  pict_url: "http://UA.localhost:9500/images-produits/", //"http://nicolasbienes_UA.ide.3wa.io:9500/images",
+  api_url: "https://github.com/NickoDev974/UA-back-V2.git", //"http://UA.localhost:9500", //"http://nicolasbienes_UA.ide.3wa.io:9500",
+  pict_url: "https://github.com/NickoDev974/UA-back-V2.git", //"http://UA.localhost:9500/images-produits/", //"http://nicolasbienes_UA.ide.3wa.io:9500/images",
 };
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/UA-front-V2/", // Assurez-vous que ceci est correct
+  base: "/UA-front-V2/",
   assetsInclude: [
     "**/*.jpg",
     "**/*.jpeg",
@@ -12,5 +12,5 @@ export default defineConfig({
     "**/*.gif",
     "**/*.svg",
     "**/*.JPG",
-  ], // Ajoutez cette ligne
+  ],
 });
