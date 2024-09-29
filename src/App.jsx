84e92901f41b17c27365a88b45626bd98be483bc
Nicolas97431 <nickogsm@gmail.com>
2024-09-29 16:22:27 +0400

    Updated vite config and added vercel.json for Vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ import StoreMarie from "./containers/storeMarie";
 import Footer from "./components/footer";
 
 import "./App.scss";
-import { Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import RequireAuth from "./helpers/require-auth";
 import AddCategory from "./containers/admin/category/addCategory";
 import Calcul from "./containers/calcul";
@@ -31,102 +31,90 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 function App() {
   return (
     <div>
-      <Router basename="/UA-front-V2">
-        <Header />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <RequireAuth
-                child={Home}
-                auth={false}
-                admin={false}
-                prepa={false}
-              />
-            }
-          />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/cgv" element={<Cgv />} />
-          <Route
-            path="/profil"
-            element={<RequireAuth child={Profil} auth={true} admin={false} />}
-          />
-          <Route
-            path="/product"
-            element={<RequireAuth child={Product} auth={false} admin={false} />}
-          />
-          <Route
-            path="/calcul"
-            element={<RequireAuth child={Calcul} auth={false} admin={false} />}
-          />
-          <Route
-            path="/storePierre"
-            element={
-              <RequireAuth child={StorePierre} auth={false} admin={false} />
-            }
-          />
-          <Route
-            path="/storePaul"
-            element={
-              <RequireAuth child={StorePaul} auth={false} admin={false} />
-            }
-          />{" "}
-          <Route
-            path="/storeMarie"
-            element={
-              <RequireAuth child={StoreMarie} auth={false} admin={false} />
-            }
-          />
-          <Route
-            path="/detail/:id"
-            element={<RequireAuth child={Detail} auth={false} admin={false} />}
-          />
-          <Route
-            path="/admin"
-            element={<RequireAuth child={Admin} auth={true} admin={true} />}
-          />
-          <Route
-            path="/addProduct"
-            element={
-              <RequireAuth child={AddProduct} auth={true} admin={true} />
-            }
-          />
-          <Route
-            path="/editProduct/:id"
-            element={
-              <RequireAuth child={EditProduct} auth={true} admin={true} />
-            }
-          />
-          <Route
-            path="/addCategory"
-            element={
-              <RequireAuth child={AddCategory} auth={true} admin={true} />
-            }
-          />
-          <Route
-            path="/basket"
-            element={<RequireAuth child={Basket} auth={false} admin={false} />}
-          />
-          <Route
-            path="/payment/:orderId"
-            element={<RequireAuth child={Payment} auth={true} admin={false} />}
-          />
-          <Route
-            path="/success"
-            element={<RequireAuth child={Success} auth={true} admin={false} />}
-          />
-          <Route
-            path="/orderDetail/:id"
-            element={
-              <RequireAuth child={OrderDetail} auth={true} admin={true} />
-            }
-          />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <RequireAuth
+              child={Home}
+              auth={false}
+              admin={false}
+              prepa={false}
+            />
+          }
+        />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cgv" element={<Cgv />} />
+        <Route
+          path="/profil"
+          element={<RequireAuth child={Profil} auth={true} admin={false} />}
+        />
+        <Route
+          path="/product"
+          element={<RequireAuth child={Product} auth={false} admin={false} />}
+        />
+        <Route
+          path="/calcul"
+          element={<RequireAuth child={Calcul} auth={false} admin={false} />}
+        />
+        <Route
+          path="/storePierre"
+          element={
+            <RequireAuth child={StorePierre} auth={false} admin={false} />
+          }
+        />
+        <Route
+          path="/storePaul"
+          element={<RequireAuth child={StorePaul} auth={false} admin={false} />}
+        />{" "}
+        <Route
+          path="/storeMarie"
+          element={
+            <RequireAuth child={StoreMarie} auth={false} admin={false} />
+          }
+        />
+        <Route
+          path="/detail/:id"
+          element={<RequireAuth child={Detail} auth={false} admin={false} />}
+        />
+        <Route
+          path="/admin"
+          element={<RequireAuth child={Admin} auth={true} admin={true} />}
+        />
+        <Route
+          path="/addProduct"
+          element={<RequireAuth child={AddProduct} auth={true} admin={true} />}
+        />
+        <Route
+          path="/editProduct/:id"
+          element={<RequireAuth child={EditProduct} auth={true} admin={true} />}
+        />
+        <Route
+          path="/addCategory"
+          element={<RequireAuth child={AddCategory} auth={true} admin={true} />}
+        />
+        <Route
+          path="/basket"
+          element={<RequireAuth child={Basket} auth={false} admin={false} />}
+        />
+        <Route
+          path="/payment/:orderId"
+          element={<RequireAuth child={Payment} auth={true} admin={false} />}
+        />
+        <Route
+          path="/success"
+          element={<RequireAuth child={Success} auth={true} admin={false} />}
+        />
+        <Route
+          path="/orderDetail/:id"
+          element={<RequireAuth child={OrderDetail} auth={true} admin={true} />}
+        />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/UA-front-V2/",
+  base: "/", // pour github pages :"/UA-front-V2/",
   assetsInclude: [
     "**/*.jpg",
     "**/*.jpeg",
